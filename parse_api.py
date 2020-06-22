f99d80f1377e731059cfabff0b4bd9130921b4eb
patrickemacd <patrickmacdonald@Patricks-MacBook-Pro.local>
2020-06-22 17:34:45 -0400

    added for loop for album output

--- parse_api.py
+++ parse_api.py
@@ -28,3 +28,21 @@ album_1 = spotify.get_album(str(album_id))
 
 # getting the track name... can also get duration as well
 album_track1 = album_1['tracks']['items'][0]['name']
+
+
+
+
+# created a little input function so I can input whatever artists I want to look into
+q_name = input("What artist are you looking for? \n Type the name here: ")
+search = spotify.search(query = str(q_name), search_type = "album", limit_on = "5")
+
+# just a little for loop to get information in more legible context
+
+for i in range(0,5):
+   album_name = search['albums']['items'][i]['name']
+   total_tracks =  search['albums']['items'][i]['total_tracks']
+   album_year = search['albums']['items'][i]['release_date']
+
+   print(f"{album_name} : {total_tracks}, released {album_year}")
+
+   
